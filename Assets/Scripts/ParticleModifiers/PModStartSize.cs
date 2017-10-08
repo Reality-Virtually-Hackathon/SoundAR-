1697e7d31ad06ff1f41e28aafb938d6ede119595
mynameisjohn <mynameisjohnj@gmail.com>
2017-10-08 00:51:08 -0400

    Set Size Slider

--- Assets/Scripts/ParticleModifiers/PModStartSize.cs
+++ Assets/Scripts/ParticleModifiers/PModStartSize.cs
@@ -2,15 +2,21 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(ParticleSystem))]
 public class PModStartSize : MonoBehaviour {
+    ParticleSystem ps;
+
+    [Range(.1f,1f)]
+    public float StartSize = 1f;
 
 	// Use this for initialization
 	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+        ps = GetComponent<ParticleSystem>();
+    }
+
+    // Update is called once per frame
+    void Update () {
+        ParticleSystem.MainModule mm = ps.main;
+        mm.startSize = StartSize;
 	}
 }
