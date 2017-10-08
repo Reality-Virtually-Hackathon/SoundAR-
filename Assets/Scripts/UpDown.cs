c0ae96e85ee4c45b1a0041dc914237a23f792c96
mynameisjohn <mynameisjohnj@gmail.com>
2017-10-08 01:41:58 -0400

    Adding some new sytsems

---
+++ Assets/Scripts/UpDown.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UpDown : MonoBehaviour
+{
+    public Vector3 ChangeBy;
+    Vector3 InitialPos;
+
+    // Use this for initialization
+    void Start()
+    {
+        InitialPos = transform.localPosition;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.localPosition = InitialPos + Mathf.Sin( Time.time ) * ChangeBy;
+    }
+}
