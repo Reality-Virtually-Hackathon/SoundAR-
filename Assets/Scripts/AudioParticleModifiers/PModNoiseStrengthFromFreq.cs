a1d8734fde3e6a601908fbfee91397bab2a005c1
mynameisjohn <mynameisjohnj@gmail.com>
2017-10-08 15:36:13 -0400

    Getting writhemic scene to work, had to add microphone to player settings

---
+++ Assets/Scripts/AudioParticleModifiers/PModNoiseStrengthFromFreq.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(ParticleSystem))]
+public class PModNoiseStrengthFromFreq : AudioReader {
+    public float Initial, Offset;
+    protected override void _OnLevelChange( float f )
+    {
+        ParticleSystem.NoiseModule nm = GetComponent<ParticleSystem>().noise;
+        nm.strengthMultiplier = (1 + 50 * f)/100f;
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
--- Assets/Scripts/MakeAudSrcMic.cs
+++ Assets/Scripts/MakeAudSrcMic.cs
@@ -9,7 +9,7 @@ public class MakeAudSrcMic : MonoBehaviour
         foreach (string s in Microphone.devices)
             Debug.Log( s );
         AudioSource aud = GetComponent<AudioSource>();
-        aud.clip = Microphone.Start( Microphone.devices[0], true, 10, 48000 );
+        aud.clip = Microphone.Start( null, true, 10, 44100 );
         aud.loop = true;
         while (!(Microphone.GetPosition( null ) > 0)) { }
         aud.Play();
